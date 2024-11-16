8acfdbea9299f6914b913c749e4061b6d5da8882
danhartme <69963090+danhartme@users.noreply.github.com>
2024-11-16 18:21:04 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CryptoNewsAnalyzer from './components/CryptoNewsAnalyzer';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CryptoNewsAnalyzer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
